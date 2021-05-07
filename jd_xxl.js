e896beb31b23c8180cab07b7c5abbc9a751c2259
jd_script <73680479+xuelang7009@users.noreply.github.com>
2021-05-07 09:51:46 +0800

    Create jd_xxl.js
    w

--- jd_xxl.js
+++ jd_xxl.js
@@ -254,7 +254,7 @@ function checkLogin() {
 async function play() {
   $.level += 1
   console.log(`当前关卡：${$.level}`)
-  while ($.strength >= 5 && $.level <= 440) {
+  while ($.strength >= 5 && $.level <= 500) {
     await beginLevel()
   }
   if($.not3Star.length && $.strength >= 5){
